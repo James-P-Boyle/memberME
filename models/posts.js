f340500802f0016ac68d49b381fe0baa275a2a33
James Boyle <james.patrick.boyle92@gmail.com>
2022-05-11 14:33:20 +0200

    Add posts model

---
+++ models/posts.js
@@ -0,0 +1,12 @@
+const { Schema, model, Types } = require("mongoose");
+
+const postsSchema = new Schema({
+  caption: { type: String, require: true, default: "Memories" },
+  img: { type: String, require: true },
+  createdAt: { type: Date, default: Date.now() },
+  /*  comments: [{ type: Types.ObjectId, ref: "Comment" }], */ // like line 7 "Post"
+});
+
+const postsModel = model("Post", postsSchema);
+
+module.exports = postsModel;
