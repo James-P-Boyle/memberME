7633164fe96e05ddd1c484aad6b8a6f6a21b0502
James Boyle <james.patrick.boyle92@gmail.com>
2022-05-11 15:24:51 +0200

    Update Userschema password select-false

--- models/users.js
+++ models/users.js
@@ -3,7 +3,7 @@ const { Schema, model } = require("mongoose");
 const usersSchema = new Schema({
   email: { type: String, require: true, unique: true },
   userName: { type: String, require: false },
-  password: { type: String, require: true },
+  password: { type: String, require: true, select: false },
   createdAt: { type: Date, default: Date.now() },
   isActive: { type: Boolean, default: true },
 });
