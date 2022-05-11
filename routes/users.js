62d04370c4badc55a057e9d986fa5d8f236df755
James Boyle <james.patrick.boyle92@gmail.com>
2022-05-11 14:27:49 +0200

    Add controllers to puser  routes

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require("express");
+const { login, signup } = require("../controllers/users");
 
 const usersRouter = express.Router();
 
-usersRouter.get("/", (req, res) => {
-  res.json("working users router");
-});
+usersRouter.post("/login", login);
+usersRouter.post("/signup", signup);
 
 module.exports = usersRouter;
