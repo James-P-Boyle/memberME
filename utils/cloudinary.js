0b42dbab3abe32b1b63323f9ae5a4b3325589442
James Boyle <james.patrick.boyle92@gmail.com>
2022-05-12 15:05:50 +0200

    Add cloudinary to .eenv

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -2,9 +2,9 @@ require("dotenv/config");
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "jamesyboyle",
-  api_key: "571366173718858",
-  api_secret: "jDlmZId2t7-w1fgqDyWzRR9lvl8",
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = { cloudinary };
