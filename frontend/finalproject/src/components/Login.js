92fe58374596f8e7aca58afe5da4f32960add86c
James Boyle <james.patrick.boyle92@gmail.com>
2022-05-16 15:20:59 +0200

    Add login logic

--- frontend/finalproject/src/components/Login.js
+++ frontend/finalproject/src/components/Login.js
@@ -1,9 +1,46 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleEmailChange = (e) => {
+    const val = e.target.value;
+    setEmail(val);
+  };
+
+  const handlePasswordChange = (e) => {
+    const val = e.target.value;
+    setPassword(val);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    logIn(email, password);
+  };
+
+  const logIn = async (email, password) => {
+    console.log(email, password);
+    try {
+      const { data } = await axios.post("http://localhost:4000/auth/login", {
+        email,
+        password,
+      });
+      localStorage.setItem("token", data);
+      navigate("/home");
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg">
         <h3 className="text-2xl font-bold text-center">Log In</h3>
-        <form action="http://localhost:4000/auth/login" method="POST">
+        <form onSubmit={handleSubmit}>
           <div className="mt-4">
             <div>
               <label htmlFor="email">Email</label>
@@ -14,6 +51,7 @@ export default function Login() {
                 id="email"
                 placeholder="Email"
                 autoComplete="on"
+                onChange={handleEmailChange}
                 className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
               />
             </div>
@@ -25,6 +63,7 @@ export default function Login() {
                 name="password"
                 id="password"
                 autoComplete="on"
+                onChange={handlePasswordChange}
                 placeholder="Password"
                 className="w-full px-4 py-2 mt-2 border rounded-md focus:outline-none focus:ring-1 focus:ring-blue-600"
               />
