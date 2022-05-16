f20b08f9b88ebc45f7cfc5695406370096d89c6b
James Boyle <james.patrick.boyle92@gmail.com>
2022-05-16 11:33:52 +0200

    Add react-router-dom

--- frontend/finalproject/src/App.js
+++ frontend/finalproject/src/App.js
@@ -1,9 +1,15 @@
+import { Routes, Route } from "react-router-dom";
+//COMPONENTS
 import UploadImage from "./components/UploadImage";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="bg-yellow-500 m-2">
-      <UploadImage />
+      <Navbar />
+      <Routes>
+        <Route path="/upload" element={<UploadImage />} />
+      </Routes>
     </div>
   );
 }
