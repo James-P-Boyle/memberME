14dfbbe10f070b9ee9e1b9b967e6c0e94d291d68
James Boyle <james.patrick.boyle92@gmail.com>
2022-05-16 11:36:32 +0200

    Add signup component

--- frontend/finalproject/src/App.js
+++ frontend/finalproject/src/App.js
@@ -2,13 +2,15 @@ import { Routes, Route } from "react-router-dom";
 //COMPONENTS
 import UploadImage from "./components/UploadImage";
 import Navbar from "./components/Navbar";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
-    <div className="bg-yellow-500 m-2">
+    <div>
       <Navbar />
       <Routes>
         <Route path="/upload" element={<UploadImage />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </div>
   );
