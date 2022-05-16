6ebc6965c6a5a8f5455729b957596aea8557ce74
James Boyle <james.patrick.boyle92@gmail.com>
2022-05-16 15:13:30 +0200

    Change title

--- frontend/finalproject/src/components/Login.js
+++ frontend/finalproject/src/components/Login.js
@@ -2,7 +2,7 @@ export default function Login() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="px-8 py-6 mt-4 text-left bg-white shadow-lg">
-        <h3 className="text-2xl font-bold text-center">Sign Up</h3>
+        <h3 className="text-2xl font-bold text-center">Log In</h3>
         <form action="http://localhost:4000/auth/login" method="POST">
           <div className="mt-4">
             <div>
