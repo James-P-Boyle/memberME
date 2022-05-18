a268ffe791196b4fa0416a740af18fa52599f370
James Boyle <james.patrick.boyle92@gmail.com>
2022-05-18 07:37:10 +0200

    Add basic timeline layout

--- frontend/finalproject/src/components/Home.js
+++ frontend/finalproject/src/components/Home.js
@@ -1,15 +1,29 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+import Timeline from "./TimeLine";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+  console.log(posts);
   useEffect(() => {
     axios
       .get("http://localhost:4000/posts", {
         headers: { authorization: `Bearer ${localStorage.getItem("token")}` },
       })
-      .then((res) => console.log(res))
+      .then((res) => setPosts(res.data))
       .catch((err) => console.log(err));
   }, []);
 
-  return <div>HOME PAGE WORKING!!!!</div>;
+  return (
+    <div className="max-w-xl border-8 mx-auto">
+      {posts.map((post, index) => (
+        <Timeline
+          key={post.index}
+          caption={post.caption}
+          img={post.img}
+          date={post.date}
+        />
+      ))}
+    </div>
+  );
 }
