f2f7c9d4aef5c1ff925f528132a8c7fc911b4cf7
James Boyle <james.patrick.boyle92@gmail.com>
2022-05-18 13:17:06 +0200

    Add fontawesome

--- frontend/finalproject/src/components/List.js
+++ frontend/finalproject/src/components/List.js
@@ -1,12 +1,15 @@
 export default function List({ caption, img, date = "", id, setImgSource }) {
   return (
     <div className="mx-auto my-2 shadow-md border md:rounded-xl">
-      <div className="flex justify-between p-3">
+      <div className="flex justify-between py-3 px-5">
         <div className="">
-          <h1 className="text-2xl capitalize">{caption}</h1>
+          <h1 className="text-xl capitalize font-bold">{caption}</h1>
           <p className="text-sm font-light">{date.split("T")[0]}</p>
         </div>
-        <button>options</button>
+        <button className="font-bold text-xl hover:animate-bounce hover:font-black">
+          ...
+          {/*  <i className="fa-solid fa-ellipsis-stroke me-1"></i> */}
+        </button>
       </div>
 
       <img
@@ -15,7 +18,7 @@ export default function List({ caption, img, date = "", id, setImgSource }) {
         }}
         src={img}
         alt=""
-        className="max-h-30 max-w-30 mx-auto"
+        className="max-h-30 max-w-30 mx-auto cursor-pointer"
       />
     </div>
   );
