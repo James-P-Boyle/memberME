deca5b89045a9fac1ced8c5ec52a6cb6f5d7fd8b
James Boyle <james.patrick.boyle92@gmail.com>
2022-05-24 13:34:46 +0200

    Add login reducers

---
+++ client/src/redux/reducers/auth.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    isAuthenticated: localStorage.getItem("token") ? true : false,
+
+    token: localStorage.getItem("token") ? localStorage.getItem("token") : "",
+    user: {},
+  },
+  reducers: {
+    authenticate: (state) => {
+      state.isAuthenticated = true;
+      state.token = action.payload;
+      state.user = action.payload.user;
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+      state.token = "";
+      state.user = {};
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { authenticate, logout } = authSlice.actions;
+
+export default counterSlice.reducer;
