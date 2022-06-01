17d3b2621a27f626aaca251d06d03327691bdef4
James Boyle <james.patrick.boyle92@gmail.com>
2022-06-01 09:47:20 +0200

    Add redux to DarkModeIcon

--- client/src/components/DarkModeIcon.js
+++ client/src/components/DarkModeIcon.js
@@ -1,16 +1,18 @@
 import { useSelector, useDispatch } from "react-redux";
+import { toggleDarkMode } from "../redux/reducers/theme";
 
 export default function DarkModeIcon() {
   const darkMode = useSelector((state) => state.darkMode);
   const dispatch = useDispatch();
-  const toggleDarkMode = () => {
-    console.log("clicked");
-    dispatch({ type: "TOGGLE_DARK_MODE" });
-  };
+
+  console.log(darkMode);
   return (
     <div
       className="text-white rounded-full h-10 w-10 bg-slate-100"
-      onClick={toggleDarkMode}
+      onClick={() => {
+        console.log("clicked");
+        dispatch(toggleDarkMode());
+      }}
     >
       {/*  <i className={darkMode ? "fa-solid fa-sun-bright" : "fa fa-moon"}></i> */}
     </div>
